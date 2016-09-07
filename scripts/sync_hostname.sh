0d88b37ab44940371366e10eb3ccee7d7dcdfbdd
ffx_amundi <francis.fachinan-ext@amundi.com>
2016-09-07 11:43:03 +0000

    Removed scripts

--- scripts/sync_hostname.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/sh
-
-WORKING_DIR="$( cd "$( dirname $0)" && pwd )"
-HOSTNAME=$(hostname | tr '[:lower:]' '[:upper:]')
-
-cp $WORKING_DIR/hosts /etc/
-sed -i '${/^127.0.0.1/d;}' /etc/hosts
-echo "127.0.0.1       $HOSTNAME" >> /etc/hosts
