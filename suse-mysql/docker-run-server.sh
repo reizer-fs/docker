b29990a7b4e049d12782976a0d1e368fc2fbac1e
ffx@X64 <ffx@x64>
2016-09-20 17:50:29 +0200

    Updated

--- suse-mysql/docker-run-server.sh
+++ suse-mysql/docker-run-server.sh
@@ -1,13 +1,48 @@
-#!/bin/sh
+#!/bin/bash
 
-ENV_DIRECTORY="/data/docker/mysql/ffx"
 
-docker run -d --name mysql-ffx -p 192.168.127.203:3306:3306 \
+help () {
+        echo "Usage: $0 hostname type(optional)"
+        echo "type = mysql apache squid samba torrent gitlab gcc"
+}
+
+if [ $# -lt 1 ] ; then
+        help && exit 1
+fi
+
+
+HOSTNAME=$1
+TYPE=$2
+case $TYPE in
+        mysql) CONTAINER="suse-mysql" ; PORTS="3306" ;;
+        apache)CONTAINER="suse-apache" ; PORTS="80 443";;
+        squid) CONTAINER="suse-squid" ; PORTS="3128" ;;
+        samba) CONTAINER="suse-samba" ; PORTS="" ;;
+        torrent) CONTAINER="suse-torrent";;
+        gitlab) CONTAINER="suse-gitlab";;
+        gcc) CONTAINER="suse-gcc";;
+        *) help && exit 1 ;;
+esac
+
+VIP=`getent hosts $HOSTNAME | awk '{print $1}'`
+ENV_DIRECTORY="/data/docker/$TYPE/$HOSTNAME"
+DATA_VOLUMES="var/lib/mysql"
+
+for i in $DATA_VOLUMES ; do
+        if [ ! -d $ENV_DIRECTORY/$i ] ; then
+                mkdir -p $ENV_DIRECTORY/$i
+        fi
+
+        if [ "$(ls -A $ENV_DIRECTORY/$i)" != "" ] ; then
+                VOLUMES="$VOLUMES -v $ENV_DIRECTORY/$i:/$i"
+        fi
+done
+
+docker run -d --name $HOSTNAME -p $VIP:3306:3306 \
 -e ADMIN_USER="admin" \
 -e ADMIN_PASS="admin" \
--e RESTRICTED_USER="wordpress" \
--e RESTRICTED_USER_PASSWORD="wordpress" \
--e RESTRICTED_DB="wordpress" \
--v $ENV_DIRECTORY/var/lib/mysql:/var/lib/mysql \
-suse-mysql
-#-v $ENV_DIRECTORY/etc/mysql:/etc/mysql \
+-e RESTRICTED_USER="dbuser" \
+-e RESTRICTED_USER_PASSWORD="userpass" \
+-e RESTRICTED_DB="db_" \
+$VOLUMES \
+$CONTAINER
