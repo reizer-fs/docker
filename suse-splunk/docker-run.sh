9167f827a07747b1d344f4d1c46987e430411c0f
xfx <root@localhost>
2017-08-14 19:33:14 +0200

    update

--- suse-splunk/docker-run.sh
+++ suse-splunk/docker-run.sh
@@ -3,7 +3,7 @@
 
 help () {
         echo "Usage: $0 hostname type(optional)"
-        echo "type = mysql apache splunk squid samba torrent gitlab gcc"
+        echo "type = mysql apache splunk splunk-light splunk-fw squid samba torrent gitlab gcc"
 }
 
 if [ $# -lt 1 ] ; then
@@ -23,35 +23,40 @@ case $TYPE in
         torrent) IMAGE="suse-torrent";;
         gitlab) IMAGE="suse-gitlab";;
         gcc) IMAGE="suse-gcc";;
-        splunk) IMAGE="suse-splunk"; PORTS="8000 443";;
-        splunk-light) IMAGE="debian-splunk-light"; PORTS="8000 443";;
+        splunk) IMAGE="suse-splunk"; PORTS="8000 443 9997";;
+        splunk-light) IMAGE="suse-splunk-light"; PORTS="8000 443";;
+        splunk-fw) IMAGE="suse-splunk-fw"; PORTS="8089";;
         *) help && exit 1 ;;
 esac
 
 VIP=`getent hosts $HOSTNAME | awk '{print $1}'`
-ENV_DIRECTORY="/data/docker/splunk/$HOSTNAME"
+ENV_DIRECTORY="/data/docker/$TYPE/$HOSTNAME"
 DATA_VOLUMES=""
 
-if [ ! -z $LINKED_CONTAINER ] ; then
-        EXTRA_OPTS="$EXTRA_OPTS --link $LINKED_CONTAINER"
+if [ ! -z "$LINKED_CONTAINER" ] ; then
+        EXTRA_OPTS="$EXTRA_OPTS --link $LINKED_CONTAINER "
 fi
 
+if [ ! -z "$PORTS" ] ; then
+	for PORT in $PORTS ; do
+        	EXTRA_OPTS="$EXTRA_OPTS -p $VIP:$PORT:$PORT "
+	done
+fi
 
-if [ ! -z $DATA_VOLUMES ] ; then
+if [ ! -z "$DATA_VOLUMES" ] ; then
 	for i in $DATA_VOLUMES ; do
 		if [ ! -d $ENV_DIRECTORY/$i ] ; then
 			mkdir -p $ENV_DIRECTORY/$i
 		fi
 
 		if [ "$(ls -A $ENV_DIRECTORY/$i)" != "" ] ; then
-			VOLUMES="$VOLUMES -v $ENV_DIRECTORY/$i:/$i"
+			VOLUMES="$VOLUMES -v $ENV_DIRECTORY/$i:/$i "
 		fi
 	done
 fi
 
 docker run -d --name $HOSTNAME \
 -h $HOSTNAME \
--p $VIP:80:8000 -p $VIP:443:443 \
 $VOLUMES \
 $EXTRA_OPTS \
 -e SPLUNK_START_ARGS="--accept-license --answer-yes --no-prompt" \
