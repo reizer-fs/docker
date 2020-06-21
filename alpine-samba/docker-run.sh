cf45a7b8828505a807518fa7bda07719a11a69f1
Sebastian Petrovich <sebastianpetrovich@gmail.com>
2020-06-21 18:35:52 -0400

    feat: added docker start script

--- alpine-samba/docker-run.sh
+++ alpine-samba/docker-run.sh
@@ -1,7 +1,7 @@
 #!/bin/bash -x
 
 NAME="alpine-samba"
-IP_VHOST="10.11.12.1"
+IP_VHOST="10.11.12.105"
 #IP_VHOST=`getent hosts $VIP | awk '{print $1}'`
 
 ##### Mount Options ####
@@ -11,5 +11,5 @@ VOL_OPTS='-v /home/reizer/Softs:/data/Softs'
 MOUNT_OPTS='-s Softs;/data/Softs;yes;no;yes'
 PORTS_OPTS="-p $IP_VHOST:137:137 -p $IP_VHOST:139:139 -p $IP_VHOST:445:445"
 AUTH_OPTS='-u reizer;badpass -n'
-docker run -d -h $NAME --name $NAME $VOL_OPTS $PORTS_OPTS alpine-samba $MOUNT_OPTS $AUTH_OPTS
+docker run -d -it -h $NAME --name $NAME $VOL_OPTS $PORTS_OPTS localhost/alpine-samba $MOUNT_OPTS $AUTH_OPTS
 #docker run -d -it -h $NAME --name $NAME $VOL_OPTS $PORTS_OPTS --entrypoint "/bin/bash" alpine-samba
--- alpine-squid/entrypoint.sh
+++ alpine-squid/entrypoint.sh
@@ -22,13 +22,7 @@ create_cache_dir () {
 }
 
 create_log_dir
-create_cache_dir
+#create_cache_dir
 
-if [[ -z ${1} ]]; then
-	if [[ ! -d ${SQUID_CACHE_DIR}/00 ]]; then
-		echo "Initializing cache..."
-		create_cache_dir || echo "Initializing cache failed, retrying ..." && create_cache_dir
-	fi
-	echo "Starting squid..."
-	start_squid
-fi
+echo "Starting squid..."
+start_squid
