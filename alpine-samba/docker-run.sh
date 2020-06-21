2357b6c9e4094968cc83ef78e0261a0e6f577a17
Sebastian Petrovich <sebastianpetrovich@gmail.com>
2020-06-22 00:38:23 +0200

    feat: added docker run script example

--- alpine-samba/docker-run.sh
+++ alpine-samba/docker-run.sh
@@ -5,11 +5,11 @@ IP_VHOST="10.11.12.1"
 #IP_VHOST=`getent hosts $VIP | awk '{print $1}'`
 
 ##### Mount Options ####
-#  name;/path[;browsable;readonly;guest;users;admins]"
+#  name;/path[;browsable;readonly;guest;users;admins;writelist;comment]"
 ####
-VOL_OPTS='-v /home/reizer/Softs:/data/Softs'
-MOUNT_OPTS='-s Softs;/data/Softs;yes;no;yes'
+VOL_OPTS='-v /home/reizer/Games:/data/Games'
+MOUNT_OPTS='-s Games;/data/Games;yes;yes;yes;reizer;reizer;reizer'
 PORTS_OPTS="-p $IP_VHOST:137:137 -p $IP_VHOST:139:139 -p $IP_VHOST:445:445"
 AUTH_OPTS='-u reizer;badpass -n'
-docker run -d -h $NAME --name $NAME $VOL_OPTS $PORTS_OPTS alpine-samba $MOUNT_OPTS $AUTH_OPTS
+sudo docker run -d -h $NAME --name $NAME $VOL_OPTS $PORTS_OPTS alpine-samba $MOUNT_OPTS $AUTH_OPTS
 #docker run -d -it -h $NAME --name $NAME $VOL_OPTS $PORTS_OPTS --entrypoint "/bin/bash" alpine-samba
