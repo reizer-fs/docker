4d5b3efc16eedf61f5d18e55a5d4702ead9c8170
Sebastian Petrovich <sebastianpetrovich@gmail.com>
2020-11-14 18:09:04 +0100

    fix: added samba script

---
+++ alpine-samba/docker-run.sh
@@ -0,0 +1,42 @@
+DIRECTORY="$HOME"
+SHARES="Downloads Git Documents"
+IP_VHOST="10.11.66.1"
+DOCKER_CMD="podman"
+DOCKER_ARGS="--cpus=1 -m 1g"
+DOCKER_IMG_NAME="alpine-samba"
+DOCKER_CNT_NAME="--name alpine-samba"
+DOCKER_PORTS_BIND="-p $IP_VHOST:137:137 -p $IP_VHOST:139:139 -p $IP_VHOST:445:445"
+DOCKER_CMD_ARGS="-n -u ffx;badpass"
+
+# Allow users to bind on low ports
+sudo sysctl -w net.ipv4.ip_unprivileged_port_start=0 &>/dev/null
+
+# Allow users to limit cpu in systemd configuration (rootless)
+if [ ! -f "/etc/systemd/system/user\@.service.d/delegate.conf" ] ; then
+    sudo mkdir /etc/systemd/system/user\@.service.d &>/dev/null
+    cat << EOF | sudo tee -a /etc/systemd/system/user\@.service.d/delegate.conf &>/dev/null
+[Service]
+Delegate=memory pids cpu io
+EOF
+sudo systemctl daemon-reload
+fi
+
+
+# Create local shares if not present
+for share in ${SHARES} ; do
+    if [ ! -d "${DIRECTORY}/${share}" ] ; then
+	mkdir -p ${DIRECTORY}/${share}
+    fi
+    DOCKER_VOLUMES="${DOCKER_VOLUMES} -v ${DIRECTORY}/${share}:/shares/${share}"
+    DOCKER_CMD_ARGS="${DOCKER_CMD_ARGS} -s ${share};/shares/${share};yes;no;yes;;ffx"
+done
+
+CMD="${DOCKER_CMD} run --rm -it -d --hostname ${DOCKER_IMG_NAME} \
+${DOCKER_ARGS} \
+${DOCKER_CNT_NAME} \
+${DOCKER_PORTS_BIND} \
+${DOCKER_VOLUMES} \
+${DOCKER_IMG_NAME} \
+${DOCKER_CMD_ARGS}"
+
+$CMD
