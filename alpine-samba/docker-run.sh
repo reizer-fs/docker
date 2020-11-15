cf18c783ac6d05b4ba40b4dd4fe8c88da2943947
Sebastian Petrovich <sebastianpetrovich@gmail.com>
2020-11-15 15:39:13 +0100

    fix: multiple fix

--- alpine-samba/docker-run.sh
+++ alpine-samba/docker-run.sh
@@ -1,34 +1,40 @@
 DIRECTORY="$HOME"
 SHARES="Downloads Git Documents"
-IP_VHOST="10.11.66.1"
+IP_VHOST="10.11.66.1 10.11.12.1"
 DOCKER_CMD="podman"
 DOCKER_ARGS="--cpus=1 -m 1g"
 DOCKER_IMG_NAME="alpine-samba"
 DOCKER_CNT_NAME="--name alpine-samba"
-DOCKER_PORTS_BIND="-p $IP_VHOST:137:137 -p $IP_VHOST:139:139 -p $IP_VHOST:445:445"
 DOCKER_CMD_ARGS="-n -u ffx;badpass"
 
 # Allow users to bind on low ports
 sudo sysctl -w net.ipv4.ip_unprivileged_port_start=0 &>/dev/null
 
+# Allow samba port on firewalld
+sudo firewall-cmd --add-service=samba --permanent
+
 # Allow users to limit cpu in systemd configuration (rootless)
 if [ ! -f "/etc/systemd/system/user\@.service.d/delegate.conf" ] ; then
     sudo mkdir /etc/systemd/system/user\@.service.d &>/dev/null
-    cat << EOF | sudo tee -a /etc/systemd/system/user\@.service.d/delegate.conf &>/dev/null
+    cat << EOF | sudo tee /etc/systemd/system/user\@.service.d/delegate.conf &>/dev/null
 [Service]
 Delegate=memory pids cpu io
 EOF
 sudo systemctl daemon-reload
 fi
 
+# Bind port on multiple interface
+for IP in ${IP_VHOST} ; do
+    DOCKER_PORTS_BIND="${DOCKER_PORTS_BIND} -p ${IP}:137:137 -p ${IP}:139:139 -p ${IP}:445:445"
+done
 
 # Create local shares if not present
-for share in ${SHARES} ; do
-    if [ ! -d "${DIRECTORY}/${share}" ] ; then
-	mkdir -p ${DIRECTORY}/${share}
+for SHARE in ${SHARES} ; do
+    if [ ! -d "${DIRECTORY}/${SHARE}" ] ; then
+	mkdir -p ${DIRECTORY}/${SHARE}
     fi
-    DOCKER_VOLUMES="${DOCKER_VOLUMES} -v ${DIRECTORY}/${share}:/shares/${share}"
-    DOCKER_CMD_ARGS="${DOCKER_CMD_ARGS} -s ${share};/shares/${share};yes;no;yes;;ffx"
+    DOCKER_VOLUMES="${DOCKER_VOLUMES} -v ${DIRECTORY}/${SHARE}:/shares/${SHARE}"
+    DOCKER_CMD_ARGS="${DOCKER_CMD_ARGS} -s ${SHARE};/shares/${SHARE};yes;no;yes;;ffx"
 done
 
 CMD="${DOCKER_CMD} run --rm -it -d --hostname ${DOCKER_IMG_NAME} \
