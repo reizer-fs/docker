76b2803626ff5d8c9aad94cd38ef23a36449b149
Sebastian Petrovich <sebastianpetrovich@gmail.com>
2021-06-12 14:01:31 +0200

    fix: create cache dir duplicate

--- alpine-squid/entrypoint.sh
+++ alpine-squid/entrypoint.sh
@@ -3,17 +3,14 @@
 set -e
 
 create_log_dir() {
-  mkdir -p ${SQUID_LOG_DIR}
-  chmod -R 755 ${SQUID_LOG_DIR}
+  mkdir -p $SQUID_LOG_DIR
+  chmod -R 755 $SQUID_LOG_DIR
   chown -R $SQUID_USER $SQUID_LOG_DIR
 }
 
-create_cache_dir() {
+create_cache_dir () {
   mkdir -p $SQUID_CACHE_DIR
   chown -R $SQUID_USER $SQUID_CACHE_DIR
-}
-
-create_cache_dir () {
         squid -z -F &
         PID=$!
         wait $(pgrep -P $PID)
