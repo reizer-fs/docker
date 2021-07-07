ef06bb7827c6813a779837fb225f9595c9ee4d65
Sebastian Petrovich <sebastianpetrovich@gmail.com>
2021-07-07 10:38:26 -0400

    feat: added new images alpine-xmrig

---
+++ alpine-xmrig/entrypoint.sh
@@ -0,0 +1,33 @@
+#!/bin/sh
+
+set -e
+
+create_log_dir() {
+  mkdir -p $SQUID_LOG_DIR
+  chmod -R 755 $SQUID_LOG_DIR
+  chown -R $SQUID_USER $SQUID_LOG_DIR
+}
+
+create_cache_dir () {
+  mkdir -p $SQUID_CACHE_DIR
+  chown -R $SQUID_USER $SQUID_CACHE_DIR
+        squid -z -F &
+        PID=$!
+        wait $(pgrep -P $PID)
+}
+
+create_log_dir
+create_cache_dir
+sleep 5
+
+# default behaviour is to launch squid
+if [[ -z ${1} ]]; then
+  if [[ ! -d ${SQUID_CACHE_DIR}/00 ]]; then
+    echo "Initializing cache..."
+    $(which squid) -N -f /etc/squid/squid.conf -z
+  fi
+  echo "Starting squid..."
+  exec $(which squid) -f /etc/squid/squid.conf -NYCd 1 ${EXTRA_ARGS}
+else
+  exec "$@"
+fi
